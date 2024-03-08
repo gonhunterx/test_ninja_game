7504d77353a11e692a9f5c167cbfe33278e7d1c5
Jadon <47489511+gonhunterx@users.noreply.github.com>
2024-03-07 19:56:15 -0500

    update

--- game.py
+++ game.py
@@ -42,6 +42,7 @@ class Game:
             'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
             'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
             'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+            'star': Animation(load_images('star'), img_dur=20, loop=True),
             'gun': load_image('gun.png'),
             'projectile': load_image('projectile.png'),
         }
